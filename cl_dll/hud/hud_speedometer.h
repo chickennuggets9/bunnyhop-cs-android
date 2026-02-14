3a68f4a2bdd5d31d7ece90c1bbb360a5f303b72b
chickennuggets9 <hehett937@gmail.com>
2026-02-14 18:55:31 +0300

    Create hud_speedometer.h from Axion's content

---
+++ cl_dll/hud/hud_speedometer.h
@@ -0,0 +1,19 @@
+// This file is part of Axion's HUD Speedometer.
+
+#ifndef HUD_SPEEDOMETER_H
+#define HUD_SPEEDOMETER_H
+
+// Include necessary headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// HUD Speedometer class
+class CHudSpeedometer {
+public:
+    void Init();
+    void Draw();
+    // Other members and methods
+};
+
+#endif // HUD_SPEEDOMETER_H
